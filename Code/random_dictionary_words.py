e06892086c1da6069eb834ac73037ee311771afb
Anna Pawl <annampawl@gmail.com>
2019-04-09 12:19:50 -0700

    refactored random dictionary words, using functions now

---
+++ Code/random_dictionary_words.py
@@ -0,0 +1,25 @@
+import sys
+import random
+
+# Program accepts one argument (# of words)
+number = input("Enter a number: ")
+
+
+def get_words(sample_text):
+    """ reads file, strips leading and trailer characters, returns list of stings (#TODO?) """
+    words = []
+    with open("sample_text.txt") as f:  # closes file when done
+        sample_text = f.read().strip().split(" ")
+        return sample_text
+
+
+def random_sentence(words):
+    """ randomly selects words from list and returns them as a properly formatted sentence (#TODO array?) """
+    random_words = random.choices(words, k=int(number))
+    random_sentence = ' '.join(random_words)  # format sentence
+    return random_sentence
+
+
+if __name__ == '__main__':
+    source = get_words('sample_text.txt')  # TODO
+    print(random_sentence(source))
