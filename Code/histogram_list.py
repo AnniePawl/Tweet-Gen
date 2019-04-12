a7436d382f9932863536a02ebdfa4c467bb0bf6c
Anna Pawl <annampawl@gmail.com>
2019-04-12 12:41:45 -0700

    refactored list of lists histogram

--- Code/histogram_list.py
+++ Code/histogram_list.py
@@ -1,25 +1,30 @@
+
 def get_words(sample_text):
-    """ reads file, strips leading and trailer characters, returns list of stings (#TODO?) """
+    """ reads file, strips leading and trailer characters, returns list of stings"""
     words = []
     with open("sample_text.txt") as f:  # closes file when done
         sample_text = f.read().strip().split(" ")
         return sample_text  # this is a list of words
 
+# Above function can also be imported!
+    # from random_dictionary_words import get_words
 
-def list_count(word_list):
+
+def histogram_list(word_list):
+    """ Takes in source text, returns histogram data structure that stores unique words and their number of occurances as a list of lists """
+    # Initialize (big)list to hold (baby)lists with type and count
     big_list = []
     for word in word_list:
+        # Look for duplicate types so count can be increased instead of adding a whole new babylist
         for baby_list in big_list:
             if word == baby_list[0]:
                 baby_list[1] += 1
-                # stop iterating if you come across word
-                break
-        # If we dont come across word, add word
+                break  # stop iterating if you come across word
         else:
             list_to_append = [word, 1]
             big_list.append(list_to_append)
     return big_list
 
 
-sample_list = get_words("sample_text.txt")
-print(tuple_dict(sample_list))
+word_list = get_words("sample_text.txt")
+print(histogram_list(word_list))
