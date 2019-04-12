94e5c169ae4ac445f9f42eb381200001bfef22eb
Anna Pawl <annampawl@gmail.com>
2019-04-12 12:48:27 -0700

    refactored histrogram dictionary

--- Code/histogram_dictionary.py
+++ Code/histogram_dictionary.py
@@ -1,13 +1,20 @@
+
 def get_words(sample_text):
-    """ reads file, strips leading and trailer characters, returns list of stings (#TODO?) """
+    """ reads file, strips leading and trailer characters, returns list of stings"""
     words = []
     with open("sample_text.txt") as f:  # closes file when done
         sample_text = f.read().strip().split(" ")
         return sample_text  # this is a list of words
 
+# Above function can also be imported!
+    # from random_dictionary_words import get_words
 
-def dictionary_histogram(word_list):  # sample text list
+
+def dictionary_histogram(word_list):
+    """ Takes in source text, returns histogram data structure that stores unique words and their number of occurances as a dictionary of key-value pairs """
+    # Initialize an empty dictionary to hold word types and counts
     histo_dict = {}
+    # First look for duplicate words(keys) and increase count (value) by one if found
     for word in word_list:
         if word in histo_dict:
             histo_dict[word] += 1
@@ -16,5 +23,5 @@ def dictionary_histogram(word_list):  # sample text list
     return histo_dict
 
 
-sample_list = get_words("sample_text.txt")
-print(tuple_dict(sample_list))
+word_list = get_words("sample_text.txt")
+print(dictionary_histogram(word_list))
